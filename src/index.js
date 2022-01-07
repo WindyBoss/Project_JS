ba585c70916c705641cdf193c6607fbd21795272
Michał <michaldziuba.praca@gmail.com>
2022-01-07 21:44:28 +0100

    Poprawione
    Dodałem listę stron.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,167 @@
+
+'use strict';
 import './sass/main.scss';
-'use strict'
-const gallery = document.querySelector(".gallery");
-const key = "9hIF6NBjrDSVNrQQJmrbBXzEzwkr0S4m";
-let page = 0;
-let country = 'pl';
-let keyword 
-function fetchEvents() {
-    fetch(`https://app.ticketmaster.com/discovery/v2/events?countryCode=${country}&sort=date,asc&page=${page}&apikey=${key}`)
-        .then(data => {
-            // console.log(data.json())
-            const response = data.json();
-            return response
-            
-        })
-        .then(response => {
-            console.log(response);
-            let pages = response.page;
-            let events = response._embedded.events;
-            console.log(events)
-            console.log(pages)
-            events.forEach(elm => {
-             gallery.insertAdjacentHTML("afterbegin",`<div class="gallery__event">
-  <div class="event__image"><img src=${elm.images[0].url} alt =""/></div>
-  <p class="event__tittle">${elm.name}</p>
-  <p class="event__date">${elm.dates.start.localDate}</p>
-  <p class="event__place">${elm._embedded.venues[0].name}</p>
-</div>`  );    
-            })
-            
-        })
-        .catch(error => {
-        console.log(error)
+const gallery = document.querySelector('.gallery');
+const key = '9hIF6NBjrDSVNrQQJmrbBXzEzwkr0S4m';
+const paginationList = document.querySelector('.pagination');
+let page=0;
+let country="pl";
+let keyword;
+
+async function galleryRender(country,page) {
+  let data = await fetchEvents(country,page);
+  console.log(data);
+  let pages = data.page;
+  let events = data._embedded.events;
+  let totalPages = pages.totalPages;
+  let links = data._links;
+  let fLink = links.first.href;
+  let lLink = links.last.href;
+  events.forEach(elm => {
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      `<div class="gallery__event">
+    <img class="event__image" src=${elm.images[0].url} alt =""/>
+    <div class="event__info">
+    <p class="event__tittle">${elm.name}</p>
+    <p class="event__date">${elm.dates.start.localDate}</p>
+    <p class="event__place"> ${elm._embedded.venues[0].name}</p></div>
+  </div>`,
+    );
+  });
+  let numPages = [];
+  for (let i = 1; i <= totalPages; i += 1) {
+    numPages.push(i);
+  }
+  numPages.forEach(elm => {
+    paginationList.insertAdjacentHTML(
+      'beforeend',
+      `<li class="pagination__link"><a href="">${elm}</a></li>`,
+    );
+  });
+  const firstLink = paginationList.children[0];
+  const lastElementIndex = paginationList.children.length - 1;
+    const lastLink = paginationList.children[lastElementIndex];
+    const link = paginationList.children;
+    
+
+}
+
+async function fetchEvents(country,page) {
+  const response = await fetch(
+    `https://app.ticketmaster.com/discovery/v2/events?countryCode=${country}&sort=date,asc&page=${page}&apikey=${key}`,
+  )
+    .then(data => {
+      // console.log(data.json())
+      const response = data.json();
+      return response;
     })
+    .catch(error => {
+      console.log(error);
+    });
+
+  return response;
 }
 
 
-fetchEvents()
+
+galleryRender(country,page);
+
+
+
+
+
+
+
+
+
+
+
+
+// // pierwsza wersja
+// import './sass/main.scss';
+// 'use strict'
+// const gallery = document.querySelector(".gallery");
+// const key = "9hIF6NBjrDSVNrQQJmrbBXzEzwkr0S4m";
+// const paginationList=document.querySelector(".pagination")
+// let page = 21;
+// let country = 'pl';
+// let keyword
+
+//  async function fetchEvents() {
+//     fetch(`https://app.ticketmaster.com/discovery/v2/events?countryCode=${country}&sort=date,asc&page=${page}&apikey=${key}`)
+//         .then(data => {
+//             // console.log(data.json())
+//             const response = data.json();
+//             return response
+            
+//         })
+//         .then(response => {
+//             console.log(response);
+//             let pages = response.page;
+//             let events = response._embedded.events;
+//             let totalPages = pages.totalPages;
+//             let links = response._links;
+//             let fLink = links.first.href;
+//             let lLink = links.last.href;
+//             console.log(lLink)
+//             console.log(fLink)
+//             console.log(links)
+//             console.log(totalPages)
+//             console.log(events)
+//             console.log(pages)
+//             events.forEach(elm => {
+//              gallery.insertAdjacentHTML("beforeend",`<div class="gallery__event">
+//   <img class="event__image" src=${elm.images[0].url} alt =""/>
+//   <div class="event__info">
+//   <p class="event__tittle">${elm.name}</p>
+//   <p class="event__date">${elm.dates.start.localDate}</p>
+//   <p class="event__place"> ${elm._embedded.venues[0].name}</p></div>
+// </div>`  );
+//             })
+//             let numPages = []
+//             for (let i = 1; i <= totalPages; i += 1){
+//                 numPages.push(i)
+//             }
+//             console.log(numPages)
+//             numPages.forEach(elm => {
+//                 paginationList.insertAdjacentHTML("beforeend",`<li class="pagination__link"><a href="">${elm}</a></li>`)
+//             })
+//             const firstLink = paginationList.children[0];
+            
+//             console.log(numPages.length)
+//             const lastElementIndex = paginationList.children.length - 1;
+//             const lastLink = paginationList.children[lastElementIndex];
+//             console.log(lastLink)
+//             console.log(lastElementIndex);
+//         //     lastLink.addEventListener("click", (event) => {
+//         //         event.preventDefault()
+//         //         gallery.innerHTML = "";
+//         //         fetch(`https://app.ticketmaster.com${fLink}page=${page}&apikey=${key}`)
+//         // .then(data => {
+//         //     // console.log(data.json())
+//         //     const response = data.json();
+//         //     return response
+            
+//         // })
+                
+                
+
+//         //     })
+//             // firstLink.childNodes[0].setAttribute("href", `${fLink}`);
+//             // console.log(firstLink.childNodes[0])
+//             // console.log(firstLink)
+//             // lastLink.childNodes[0].setAttribute("href",`${lLink}`)
+            
+//         })
+//         .catch(error => {
+//         console.log(error)
+//     })
+// }
+
+
+
+
+
+// fetchEvents()
+// // pierwsza wersja koniec
