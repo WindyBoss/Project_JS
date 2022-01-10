083425bd0fa0f03240b2baf133844f80b6a1d2db
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-01-10 19:32:57 +0100

    Prawie działa

--- src/js/form.js
+++ src/js/form.js
@@ -1,8 +1,9 @@
 import { KEY } from '..';
+import Notiflix from 'notiflix';
 
 const input = document.querySelector('.input-keyword');
 const gallery = document.querySelector('.gallery');
-const select = document.querySelector(".country");
+const select = document.querySelector('.country');
 let code = KEY;
 let keyword = '';
 
@@ -21,26 +22,69 @@ async function searchEvents(page, keyword) {
   return response;
 }
 
+async function searchCountry(page, countryCode) {
+  const response = await fetch(
+    `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${countryCode}&sort=date,asc&page=${page}&apikey=${KEY}`,
+  )
+    .then(data => {
+      const response = data.json();
+      return response;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  return response;
+}
+
 const putEvents = () => {
   keyword = input.value;
-  gallery.innerHTML = "";
-  searchEvents(0, keyword).then(data => {
-    const { page, _embedded: { events } } = data;
-    events.forEach(element => {
-      gallery.insertAdjacentHTML(
-        'beforeend',
-        `<div class="gallery__event">
+  searchEvents(0, keyword)
+    .then(data => {
+      const {
+        page,
+        _embedded: { events },
+      } = data;
+      gallery.innerHTML = '';
+      events.forEach(element => {
+        gallery.insertAdjacentHTML(
+          'beforeend',
+          `<div class="gallery__event">
       <img class="event__image" src=${element.images[0].url} alt =""/>
        <div class="event__info">
        <p class="event__tittle">${element.name}</p>
        <p class="event__date">${element.dates.start.localDate}</p>
        <p class="event__place"> ${element._embedded.venues[0].name}</p></div>
         </div>`,
-      );
-    });
-    
-  });
+        );
+      });
+    })
+    .catch(Notiflix.Notify.failure('There are no that kind of events'));
+};
+
+const putCountryEvents = () => {
+  gallery.innerHTML = '';
+  searchCountry(0, select.value)
+    .then(data => {
+      const {
+        page,
+        _embedded: { events },
+      } = data;
+      events.forEach(element => {
+        gallery.insertAdjacentHTML(
+          'beforeend',
+          `<div class="gallery__event">
+      <img class="event__image" src=${element.images[0].url} alt =""/>
+       <div class="event__info">
+       <p class="event__tittle">${element.name}</p>
+       <p class="event__date">${element.dates.start.localDate}</p>
+       <p class="event__place"> ${element._embedded.venues[0].name}</p></div>
+        </div>`,
+        );
+      });
+    })
+    .catch(Notiflix.Notify.failure('There are no events in this country'));;
 };
 
 input.addEventListener('change', putEvents);
-
+select.addEventListener('change', putCountryEvents);
